91b35d824d1ba41ce7d71d9324fcf885da7ad688
Jajo2003 <nikajarji4@gmail.com>
2025-04-23 15:53:22 +0400

    Updated frontend and it's logic

--- project/index/userPage/user.js
+++ project/index/userPage/user.js
@@ -11,6 +11,52 @@ document.addEventListener('DOMContentLoaded',function(){
         registerForm.classList.remove('hiddenForm');
         loginForm.classList.add('hiddenForm');
     });
+    
+    registerForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        const firstName = document.getElementById("FirstName").value.trim();
+        const lastName = document.getElementById("LastName").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+        const university = document.getElementById("university").value.trim();
+        if(password != confirmPassword)
+        {
+            alert("Password should match");
+            return;
+        }
+        const userData = {
+            firstName,
+            lastName,
+            email,
+            password,
+            university
+          };
+
+          fetch("backend",{
+            method:'POST',
+            headers: 
+            {
+                "Content-Type" : 'application/json',
+            },
+            body:JSON.stringify(userData)
+          });
+          
+        
+    })
+    loginForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        const email = document.getElementById("loginemail").value.trim();
+        const password = document.getElementById("loginpassword").value;
+
+        const LoginData = 
+        {
+            mail : email,
+            pass : password
+        }
+
+    })
+    
 
 
 });
